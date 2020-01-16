f479eb4b4cd5c6242685606060cd449ee82a0f23
itsjuniordias <juniordias_@live.com>
2020-01-16 18:28:11 -0300

    List Users in Main

--- appmobile/src/pages/Main/index.js
+++ appmobile/src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View } from 'react-native';
+import { View, FlatList, TouchableOpacity } from 'react-native';
 import api from '../../services/api';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -7,6 +7,13 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form, Text, SubmitButton } from './styles';
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      docs: [],
+    }
+  }
 
 
  handleNavigate = () => {
@@ -24,8 +31,18 @@ export default class Main extends Component {
 
    const { docs } = response.data;
    
-   console.log(docs);
+   this.setState({docs});
  }
+
+ renderItem = ({item}) => (
+   <View> 
+     <Text>Nome - {item.nome} </Text>
+     <Text>CPF - {item.cpf}</Text>
+      <TouchableOpacity onPress={() => {}}> 
+       <Text>Editar cadastro</Text>
+      </TouchableOpacity>
+   </View>
+ )
 
  render() {
   return (
@@ -36,6 +53,14 @@ export default class Main extends Component {
         <Icon name="person-add" size={25} color="#fff"/>
       </SubmitButton>
       </Form>
+
+      <View> 
+        <FlatList
+          data={this.state.docs}
+          keyExtractor = {item => item._id}
+          renderItem={this.renderItem}
+        /> 
+      </View>
     </Container>
   );
  }
