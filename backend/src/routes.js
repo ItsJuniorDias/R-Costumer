ff1768296c0feb125296dca71795bc4d5c9b03b7
itsjuniordias <juniordias_@live.com>
2020-01-15 22:24:58 -0300

    Middleware checkUserExists

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,21 @@
 const {Router} = require('express');
 const UserController = require('./controllers/UserController');
 
+
 const routes = Router();
+
+function checkUserExists(req, res, next) {
+  if (!req.body.nome) {
+    return res.status(400).json({ error: "Nome do usuário e obrigatório "})
+  }
+
+  return next();
+}
 
 routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 
-routes.post('/users', UserController.store);
+routes.post('/users', checkUserExists,UserController.store);
 routes.delete('/users/:id', UserController.destroy);
 routes.put('/users/:id', UserController.update);
 
