4c768ff5774cc5e275be9fcdcea3319213690a89
itsjuniordias <juniordias_@live.com>
2020-01-17 13:27:21 -0300

    Request Api POST

--- appmobile/src/pages/User/index.js
+++ appmobile/src/pages/User/index.js
@@ -1,34 +1,161 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {Component, useState} from 'react';
+import { View,  } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Form, SubmitButton, Input, ViewIcon } from './styles';
+import api from "../../services/api";
 
-export default function User() {
+import { 
+  Container, 
+  Form, 
+  SubmitButton, 
+  Input, 
+  ViewIcon, 
+  Text,
+  SubmitButtonSalvar, 
+  SubmitButtonDeletar } from './styles';
+
+export default class User extends Component {
+   constructor(props) {
+    super(props);
+
+    this.state = {
+      newUser: "",
+      users: [],
+      nome: "",
+      dataNascimento: "",
+      cpf: "",
+      celular: "",
+      email: "",
+      endereco: "",
+      obervacoes: "",
+    }
+  }
+
+  componentDidMount() {
+    this.handleAddUser();
+  }
+
+  handleAddUser = async () => {
+
+    params = {
+      nome:"Rauan",
+      dataNascimento: 12,
+      cpf: 12,
+      celular: 13,
+      email: "rauan@live.com",
+      endereco: "Rua Gs",
+      observacoes: "Já fez"
+    }
+
+     const response = await api.post('/users', params);
+     
+     console.log(response.data);
+  }
+
+  render() {
+    const {users, newUser, nome, dataNascimento, cpf, celular,email, endereco, observacoes } = this.state;
+
   return (
     <Container> 
       <Form>
+
         <Icon name="person-outline" size={35} color="#333"/>
-    
+
         <Input 
           keyboardType="default"
           autoCorrect={false}
           placeholder= "Digite seu nome"
+          returnKeyType="next"
+          required
+          value={nome}
+          onChangeText={text => this.setState({ nome: text })}
         />
+    
         
         <Icon name="date-range" size={35} color="#333"/>
         <Input 
           keyboardType="default"
           autoCorrect={false}
           placeholder= "Data Nascimento"
+          keyboardType="number-pad"
+          returnKeyType="next"
+          required
+          value={dataNascimento}
+          onChangeText={text => this.setState({ dataNascimento: text })}
         />
 
-        <SubmitButton onPress={() =>{}}>
+        <Icon name="fingerprint" size={35} color="#333"/>
+        <Input 
+          keyboardType="default"
+          autoCorrect={false}
+          placeholder= "CPF"
+          keyboardType="number-pad"
+          returnKeyType="next"
+          required
+          value={cpf}
+          onChangeText={text => this.setState({ cpf: text })}
+     
+        />
 
-        </SubmitButton>
+        <Icon name="call" size={35} color="#333"/>
+        <Input 
+          keyboardType="default"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          placeholder= "Celular"
+          returnKeyType="next"
+          required
+          value={celular}
+          onChangeText={text => this.setState({ celular: text })}
+      
+        />
+
+        <Icon name="mail-outline" size={35} color="#333"/>
+        <Input 
+          keyboardType="default"
+          autoCorrect={false}
+          keyboardType="email-address"
+          placeholder= "Email"
+          returnKeyType="next"
+          required
+          value={email}
+          onChangeText={text => this.setState({ email: text })}
+     
+        />
+
+        <Icon name="near-me" size={35} color="#333"/>
+        <Input 
+          keyboardType="default"
+          autoCorrect={false}
+          placeholder= "Endereco"
+          returnKeyType="next"
+          required
+          value={endereco}
+          onChangeText={text => this.setState({ endereco: text })}
+     
+        />
+
+        <Icon name="border-color" size={35} color="#333"/>
+        <Input 
+          keyboardType="default"
+          autoCorrect={false}
+          placeholder= "Observações"
+          returnKeyType="send"
+          value={observacoes}
+          onChangeText={text => this.setState({ observacoes: text })}
+       
+        />
+
+        <SubmitButtonSalvar  onPress={this.handleAddUser}>
+          <Text> Salvar </Text>
+        </SubmitButtonSalvar>
+        <SubmitButtonDeletar onPress={() =>{}}>
+            <Text>Deletar </Text>
+            </SubmitButtonDeletar>
       </Form>
     </Container>
   );
+  }
 }
 
 
--- appmobile/src/pages/User/styles.js
+++ appmobile/src/pages/User/styles.js
@@ -3,12 +3,12 @@ import { RectButton} from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   padding: 0 15px;
-  margin-top: 20px;
+  margin-top: 50px;
 `;
 
 
 export const Form = styled.View`
- flex-direction: column;
+flex-direction: column; 
  padding-bottom: 20px;
  border-bottom-width: 1px;
  border-color: #ddd;
@@ -19,20 +19,38 @@ export const Text = styled.Text`
  color: #333;
  height: 40px; 
  padding: 0 15px;
+ color: #fff;
+ margin-top: 5px;
 `;
 
-export const SubmitButton = styled(RectButton)`
+export const SubmitButtonSalvar = styled(RectButton)`
  justify-content: center;
  align-items: center;
  background: #3d3dff;
  border-radius: 4px;
- margin-left: 120px;
- padding: 0 8px;
+ margin-left: 20px;
+ padding: 0 10px;
+ margin-top: 50px;
 `;
 
+export const SubmitButtonDeletar = styled(RectButton)`
+ justify-content: center;
+ align-items: center;
+ background: #ff1400;
+ border-radius: 4px;
+ margin-left: 20px;
+ padding: 0 10px;
+ margin-top: 10px;
+`;
+
+
 export const Input = styled.TextInput`
+ justify-content: center;
+ align-items: center;
  font-size: 15px;
- margin-left: 50px;
+ margin-left: 40px;
+ margin-top: -30px;
  color: #333;
+ padding: 0 10px 20px 10px;
 `;
 
